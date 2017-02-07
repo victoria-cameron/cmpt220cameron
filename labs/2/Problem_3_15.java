c51df6eb2d6dfc6cac27d7271a1e0d4c34e18150
victoria cameron <victoria.cameron2@marist.edu>
2017-02-07 16:56:11 -0500

    Added in the needed if statements so that the code checks for matches among all three didgits for wins.

--- labs/2/Problem_3_15.java
+++ labs/2/Problem_3_15.java
@@ -35,12 +35,21 @@ public class Problem_3_15{
 	if (playerGuess == originalLottery) 
       System.out.println("Exact match: you win $10,000");
 	else if (guessDigit2 == lotteryDigit1 
-      && guessDigit1 == lotteryDigit2) 
-      System.out.println("Match all digits: you win $3,000"); 
+      ||guessDigit3 == lotteryDigit1 
+      && guessDigit1 == lotteryDigit2
+      ||guessDigit3 == lotteryDigit2
+      && guessDigit1 == lotteryDigit3
+      || guessDigit2 == lotteryDigit3)	  
+      System.out.println("Match two digits: you win $3,000"); 
     else if (guessDigit1 == lotteryDigit1 
       || guessDigit1 == lotteryDigit2 
+      || guessDigit1 == lotteryDigit3
       || guessDigit2 == lotteryDigit1 
-      || guessDigit2 == lotteryDigit2) 
+      || guessDigit2 == lotteryDigit2
+      || guessDigit2 == lotteryDigit3
+      || guessDigit3 == lotteryDigit1
+      || guessDigit3 == lotteryDigit2
+      || guessDigit3 == lotteryDigit3) 
       System.out.println("Match one digit: you win $1,000");
     else
       System.out.println ("Sorry, no match");
