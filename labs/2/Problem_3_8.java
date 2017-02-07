aef0946cfcbaa2c31412d10882f99c1285654b87
victoria cameron <victoria.cameron2@marist.edu>
2017-02-07 13:41:33 -0500

    I have started problem 3.8 in the lab questions and I am going to finish it soon holefully.

---
+++ labs/2/Problem_3_8.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class Problem_3_8{
+  public static void main (String [] args){
+    Scanner input = new Scanner(System.in);
+    //get numbers from user
+
+    //sort the numbers in incresing order
+	
+    //print the numbers in their new order
+    
