1fc2d953e46b6ff2b1a4313fde356a8e8b449c0f
victoria cameron <victoria.cameron2@marist.edu>
2017-02-07 16:38:18 -0500

    I have rewriten most of the lottery code to include the third digit, will change the if statements to check for this change.

---
+++ labs/2/Problem_3_15.java
@@ -0,0 +1,48 @@
+import java.util.Scanner; 
+public class Problem_3_15{
+  public static void main(String[] args) { 
+  // Generate a lottery number 
+    int originalLottery = (int)(Math.random() * 1000); 
+    
+    // Prompt the user to enter a guess 
+    Scanner input = new Scanner(System.in); 
+    System.out.print("Enter your lottery pick (three digits): "); 
+    int playerGuess = input.nextInt(); 
+
+    System.out.println("The lottery number is " + originalLottery);
+
+    int lottery = originalLottery; 
+    int guess = playerGuess;
+
+    // Get digits from lottery 
+    int lotteryDigit1 = lottery % 10;
+    lottery = lottery / 10;
+    int lotteryDigit2 = lottery % 10;
+    lottery = lottery / 10;
+    int lotteryDigit3 = lottery % 10;
+    lottery = lottery / 10; 
+
+    // Get digits from guess 
+    int guessDigit1 = guess % 10;
+    guess = guess / 10;
+    int guessDigit2 = guess % 10;
+    guess = guess / 10;
+    int guessDigit3 = guess % 10;
+    guess = guess / 10; 
+ 
+	
+    // Check the guess 
+	if (playerGuess == originalLottery) 
+      System.out.println("Exact match: you win $10,000");
+	else if (guessDigit2 == lotteryDigit1 
+      && guessDigit1 == lotteryDigit2) 
+      System.out.println("Match all digits: you win $3,000"); 
+    else if (guessDigit1 == lotteryDigit1 
+      || guessDigit1 == lotteryDigit2 
+      || guessDigit2 == lotteryDigit1 
+      || guessDigit2 == lotteryDigit2) 
+      System.out.println("Match one digit: you win $1,000");
+    else
+      System.out.println ("Sorry, no match");
+  }
+}
