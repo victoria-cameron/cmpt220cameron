1b5aa3d15ac9584b34b4b86fec8af0d30f5ad2ab
victoria cameron <victoria.cameron2@marist.edu>
2017-02-07 15:30:02 -0500

    I have changes the code for problem 3.4 back to if statements after being informed that I couldn't use switches for this problem.

--- labs/2/Problem_3_4.java
+++ labs/2/Problem_3_4.java
@@ -6,20 +6,31 @@ public class Problem_3_4{
     int month = (int) ((Math.random() * 12) +1);    
 
     //display the month (use switch?)
-    switch (month){
-      case 1: System.out.println ("January"); break;
-      case 2: System.out.println ("February"); break;
-      case 3: System.out.println ("March"); break;
-      case 4: System.out.println ("April"); break;
-      case 5: System.out.println ("May"); break;
-      case 6: System.out.println ("June"); break;
-      case 7: System.out.println ("July"); break;
-      case 8: System.out.println ("August"); break;
-      case 9: System.out.println ("September"); break;
-      case 10: System.out.println ("October"); break;
-      case 11: System.out.println ("November"); break;
-      case 12: System.out.println ("December");
-	}
+	if (month == 1)
+      System.out.println ("January");
+    else if (month == 2)
+      System.out.println ("February");
+    else if (month == 3)
+      System.out.println ("March");
+    else if (month == 4)
+      System.out.println ("April");
+    else if (month == 5)
+      System.out.println ("May");
+    else if (month == 6)
+      System.out.println ("June");
+    else if (month == 7)
+      System.out.println ("July");
+    else if (month == 8)
+      System.out.println ("August");
+    else if (month == 9)
+      System.out.println ("September");
+    else if (month == 10)
+      System.out.println ("October");
+    else if (month == 11)
+      System.out.println ("November");
+    else if (month == 12)
+     System.out.println ("December");
+	
   }
 }
 	
