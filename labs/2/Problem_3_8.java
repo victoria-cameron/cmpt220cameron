dcc39081e0d4fd25cbb33e903787c16a8ce67efc
victoria cameron <victoria.cameron2@marist.edu>
2017-02-07 14:10:02 -0500

    I finally got the logic of my code to make sense but it is giving me the value of the three intergers added together. Gonna keep working at it.

--- labs/2/Problem_3_8.java
+++ labs/2/Problem_3_8.java
@@ -1,10 +1,33 @@
-import java.util.scanner;
+import java.util.Scanner;
 public class Problem_3_8{
   public static void main (String [] args){
     Scanner input = new Scanner(System.in);
     //get numbers from user
+    System.out.print ("Enter three intergers: ");
+    int numb1 = input.nextInt();
+	int numb2 = input.nextInt();
+    int numb3 = input.nextInt();
 
+    int numbX = 0; //I am using this as a placeholder because I dont want to love stored values
     //sort the numbers in incresing order
-	
+    if (numb2 < numb1 || numb3 < numb1){
+      if (numb2 < numb1){		  
+        numbX = numb1;
+        numb1 = numb2;
+        numb2 = numbX;
+      }
+      if (numb3 < numb1){
+        numbX = numb1;
+        numb1 = numb3;
+        numb3 = numbX;
+      }
+    if (numb3 < numb2){
+      numbX = numb2;
+      numb2 = numb3;
+      numb3 = numbX;	  
+	}
     //print the numbers in their new order
-    
+    System.out.print (numb1 + numb2 + numb3);
+    }
+  }
+}   
