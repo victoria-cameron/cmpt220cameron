21c23cfa461fbcc98dda9eb08817bb40ea96a26c
victoria cameron <victoria.cameron2@marist.edu>
2017-02-08 11:34:30 -0500

    added switch to problem 3.15.

--- labs/2/Problem_3_15.java
+++ labs/2/Problem_3_15.java
@@ -30,17 +30,19 @@ public class Problem_3_15{
     int guessDigit3 = guess % 10;
     guess = guess / 10; 
  
-	
+    int win = 0;
     // Check the guess 
 	if (playerGuess == originalLottery) 
-      System.out.println("Exact match: you win $10,000");
+      win = 1;
+      
 	else if (guessDigit2 == lotteryDigit1 
       ||guessDigit3 == lotteryDigit1 
       && guessDigit1 == lotteryDigit2
       ||guessDigit3 == lotteryDigit2
       && guessDigit1 == lotteryDigit3
-      || guessDigit2 == lotteryDigit3)	  
-      System.out.println("Match two digits: you win $3,000"); 
+      || guessDigit2 == lotteryDigit3)	
+      win = 2;	  
+       
     else if (guessDigit1 == lotteryDigit1 
       || guessDigit1 == lotteryDigit2 
       || guessDigit1 == lotteryDigit3
@@ -50,8 +52,17 @@ public class Problem_3_15{
       || guessDigit3 == lotteryDigit1
       || guessDigit3 == lotteryDigit2
       || guessDigit3 == lotteryDigit3) 
-      System.out.println("Match one digit: you win $1,000");
+      win = 3;
+      
     else
-      System.out.println ("Sorry, no match");
+      win = 4;
+      
+	  
+    switch (win){
+      case 1: System.out.println("Exact match: you win $10,000"); break;
+      case 2: System.out.println("Match two digits: you win $3,000"); break;
+      case 3: System.out.println("Match one digit: you win $1,000"); break;
+      case 4: System.out.println ("Sorry, no match");
+	}
   }
 }
