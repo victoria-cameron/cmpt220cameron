b8bea0c2b296f70cc145cc30d9b86da41f0f3aaa
victoria cameron <victoria.cameron2@marist.edu>
2017-03-31 14:42:21 -0400

    I have created a new prj/1 folder with a new driver file for my revised project 1 files.

---
+++ prj/1/Driver_Prj.java
@@ -0,0 +1,10 @@
+//file: Driver_Pro1.java
+//author: Victoria Cameron
+//course: CMPT 220
+//assignment: Project 1
+//due date: March 30, 2017
+//version: 1.0
+
+import java.util.Scanner;
+public class Driver_Pro1{
+  public static void main(String[] args) {
---
+++ prj/1/Driver_Prj1.java
@@ -0,0 +1,11 @@
+//file: Driver_Prj1.java
+//author: Victoria Cameron
+//course: CMPT 220
+//assignment: Project 1
+//due date: March 30, 2017
+//version: 1.0
+
+import java.util.Scanner;
+public class Driver_Prj1{
+  public static void main(String[] args) {
+
