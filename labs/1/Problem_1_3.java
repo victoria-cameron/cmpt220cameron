bd911c2b2acf02ba4838c0ea1315f851648d597e
victoria cameron <victoria.cameron2@marist.edu>
2017-04-07 09:23:28 -0400

    I found a way to calculate the days till a birthday will be adding this fabulous information into my birthday class.

--- labs/1/Problem_1_3.java
+++ labs/1/Problem_1_3.java
@@ -7,19 +7,35 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Calendar;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class Problem_1_3{
   public static void main(String[] args) throws ParseException{
-    SimpleDateFormat sdf = new SimpleDateFormat ("dd/mm/yyyy");
-    Date date1 = sdf.parse("03/04/2017");
-    Date birthday = sdf.parse("03/04/2017");
 
-    if (date1.compareTo(birthday) > 0)
-      System.out.println ("Bro, your birthday passed");
-    else if (date1.compareTo(birthday) < 0)
-      System.out.println ("I also see that your birthday is comming soon");
-    else if (date1.compareTo(birthday) == 0)
-      System.out.println ("Its your birthday today!");
+    int day = 1;
+    int month = 12;
+    String year = "2017";
+    String date = String.valueOf(day);
+    String months = String.valueOf(month);
+
+    String birthdayThisYear = date + " " + months +" "+ year;
+
+    String pattern = "dd MM yyy";
+    SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+    String today = new SimpleDateFormat(pattern).format(new Date());
+    String inputString2 = birthdayThisYear;
+
+    try {
+        Date date1 = myFormat.parse(today);
+        Date date2 = myFormat.parse(inputString2);
+        long diff = date2.getTime() - date1.getTime();
+        diff = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        System.out.println ("Days: " + diff);
+    } catch (ParseException e) {
+        e.printStackTrace();
+    }
 
   }
 
